aadf77e20eabf968402291124af67c827064e906
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-26 19:20:59 -0500

    Operadores de Asignación

---
+++ main.js
@@ -0,0 +1,62 @@
+// Operadores de Asignación
+let x = 10;
+let y = 20;
+// console.log(x);
+x = y;
+console.log("Operadores de Asignación", x);
+
+// Operadores de Adición
+let a = 10;
+let b = 20;
+// Abreviación
+a += b;
+// Significado
+// x = x + y;
+console.log("Operadores de Adición", a);
+
+
+// Operadores de sustración
+let c = 10;
+let d = 20;
+// Abreviación
+c -= d;
+// Significado
+// x = x-y;
+console.log("Operadores de sustración", x);
+
+// Operadores de mutiplicación
+let e = 10;
+let f = 20;
+// Abreviación
+e *= f;
+// Significado
+// x = x*y;
+console.log("Operadores de multiplicación", e);
+
+// Operadores de divición
+let g = 10;
+let h = 20;
+// Abreviación
+g /= h;
+// Significado
+// x = x/y;
+console.log("Operadores de divición", g);
+
+// Operadores de resto
+let i = 10;
+let j = 6;
+// Abreviación
+i %= j;
+// Significado
+// x = x%y;
+console.log("Operadores de Resto", i);
+
+// Operadores de exponenciación
+let k = 10;
+let l = 6;
+// Abreviación
+k **= l;
+// Significado
+// x = x ** y;
+console.log("Operadores de exponenciación", k);
+
